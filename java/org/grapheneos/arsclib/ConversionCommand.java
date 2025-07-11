3bc4a6f9c299545c4249a7a4a4fa6ae9e01882c9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-07-11 14:50:29 +0300

    remove unused pkgInclusionFilters field
    It's not needed given that there are per-value inclusion filters already.

--- java/org/grapheneos/arsclib/ConversionCommand.java
+++ java/org/grapheneos/arsclib/ConversionCommand.java
@@ -4,7 +4,6 @@ public class ConversionCommand {
     String unpackedOsImageDir;
 
     Filters pkgExclusionFilters;
-    Filters pkgInclusionFilters;
     Filters exclusionFilters = new Filters();
     Filters inclusionFilters = new Filters();
     String outDir;
