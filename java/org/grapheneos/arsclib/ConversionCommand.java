fbafcdd480de9d17e20d6c28fcf52368abdda92c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-07-12 20:39:11 +0300

    add support for creating synthetic overlays
    Synthetic overlay is created by copying resources from a regular (non-overlay) package.

--- java/org/grapheneos/arsclib/ConversionCommand.java
+++ java/org/grapheneos/arsclib/ConversionCommand.java
@@ -1,7 +1,12 @@
 package org.grapheneos.arsclib;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConversionCommand {
     String unpackedOsImageDir;
+
+    List<ApkConverter.SyntheticOverlaySpec> syntheticOverlays = new ArrayList<>();
 
     Filters pkgExclusionFilters;
     Filters exclusionFilters = new Filters();
